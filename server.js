a43608557ed50f23658aec057db2794e7e7dc6c9
kaiq913 <77793337+kaiq913@users.noreply.github.com>
2023-02-16 18:00:50 -0500

    trying

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/http');
+  res.setHeader('Content-Type', 'text/html');
   res.end(data);
 });
 
